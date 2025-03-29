e81b0ee47edba95fc5369b1bd89188b1d330f09f
Shubham Prajapati <prajapatishubhama2608@gmail.com>
2025-03-29 09:23:02 +0530

    Second commit

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,6 +1,7 @@
 const captainModel = require('../models/captain.model');
 const captainService = require('../services/captain.service');
-const blackListTokenModel = require('../models/blackListToken.model');
+// const blackListTokenModel = require('../models/blackListToken.model');
+const blackListTokenModel = require("../models/blacklistToken.model");
 const { validationResult } = require('express-validator');
 
 
